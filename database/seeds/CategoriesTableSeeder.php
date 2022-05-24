7cab86a19940aca882b2fae64e34a008a194b2d5
Vitto2623 <vtr.1@icloud.com>
2022-05-24 14:55:24 +0200

    creo seeder tra i post e le categorie

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $categories = [
+            'politica',
+            'avventura',
+            'sport',
+            'attualità',
+            'environments',
+            'social',
+        ];
+
+        for ($i=0; $i < count($categories); $i++) { 
+            $newCategory = new Category();
+            $newCategory->name = $categories[$i];
+            $newCategory->color = $faker->unique->hexColor();
+            $newCategory->save();
+        }
+    }
+}
